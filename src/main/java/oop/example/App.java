9aa6ed13fd3f833766a583c3fd64a85e0e23553f
MicahPB01 <micahpuccioball@gmail.com>
2021-05-31 22:05:14 -0400

    Updated

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -6,14 +6,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner getFirstNumber = new Scanner(System.in);
-        Scanner getSecondNumber =  new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print( "What is the first number? ");
-        String firstNumber = getFirstNumber.nextLine();
+        String firstNumber = input.nextLine();
 
         System.out.print("What is the second number? ");
-        String secondNumber = getSecondNumber.nextLine();
+        String secondNumber = input.nextLine();
 
         //convert string to int
         int nOne = Integer.parseInt(firstNumber);
